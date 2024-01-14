33fb11f70eaa96e04c0133d4bb7c75c361a4aa32
前田宜宏 <>
2024-01-14 15:28:39 +0900

    Devise の導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   validates :account, presence: true, uniqueness: { case_sensitive: false }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'articles#index' #追記
+  devise_for :users # 追加
   resources :articles #追記
   resources :sample_articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
